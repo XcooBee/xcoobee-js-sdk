e96f4b7f39bd7ad74662ea1feea792202b7eb765
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2018-09-28 10:34:23 -0600

    Remove Parts of the SDK that Need Not be Exposed

--- src/xcoobee/sdk/index.js
+++ src/xcoobee/sdk/index.js
@@ -1,17 +1,9 @@
 import Config from './Config';
 import ConfigUtils from './ConfigUtils';
-import ErrorResponse from './ErrorResponse';
-import Response from './Response';
 import Sdk from './Sdk';
-import SdkUtils from './SdkUtils';
-import SuccessResponse from './SuccessResponse';
 
 export default {
   Config,
   ConfigUtils,
-  ErrorResponse,
-  Response,
   Sdk,
-  SdkUtils,
-  SuccessResponse,
 };
