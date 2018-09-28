7dbe2f5cee58ebff95dc6f1425def1bc4f147ca9
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2018-09-28 10:33:39 -0600

    Add New Unit Test

--- __tests__/unit/src/xcoobee/core/XcooBeeError.spec.js
+++ __tests__/unit/src/xcoobee/core/XcooBeeError.spec.js
@@ -4,13 +4,25 @@ describe('XcooBeeError', function () {
 
   describe('constructor', function () {
 
-    describe('called with whatever', function () {
+    describe('called with no arguments', function () {
 
       it('should construct the expected instance', function () {
         let error = new XcooBeeError();
 
         expect(error).toBeInstanceOf(XcooBeeError);
         expect(error.message).toBe('');
+        expect(error.name).toBe('XcooBeeError');
+      });
+
+    });
+
+    describe('called with one argument', function () {
+
+      it('should construct the expected instance', function () {
+        let error = new XcooBeeError('Something bad happened.');
+
+        expect(error).toBeInstanceOf(XcooBeeError);
+        expect(error.message).toBe('Something bad happened.');
         expect(error.name).toBe('XcooBeeError');
       });
 
